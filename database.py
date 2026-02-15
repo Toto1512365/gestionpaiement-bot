a6b81eef280a9a802aa102608e6ec27f5665a14b
Toto1512365 <thomas1512.rodriguez@yandex.com>
2026-02-15 10:51:41 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,14 @@
-print("DATABASE_URL:", os.environ.get('DATABASE_URL')[:50])  # affiche les 50 premiers caractères
+import os
+import psycopg2
+from datetime import datetime
+
+class Database:
+    def __init__(self):
+        self.conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
+        self.c = self.conn.cursor()
+        self._init_db()
+
+    def _init_db(self):
+        # ... (le reste du code est inchangé)
+      
+      print("DATABASE_URL:", os.environ.get('DATABASE_URL')[:50])  # affiche les 50 premiers caractères
